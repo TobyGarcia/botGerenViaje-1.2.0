a6d19d0a87c3581a3d3958dd8427cba451279de4
TobyGarcia <tvrstudios00@gmail.com>
2026-09-05 08:50:00 -0600

    pequeña mejora en el vite.config del proyecto

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -46,6 +46,8 @@ export default defineConfig({
         orientation: 'portrait'
       },
       workbox: {
+        skipWaiting: true,
+        clientsClaim: true,
         globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
         runtimeCaching: [
           {
